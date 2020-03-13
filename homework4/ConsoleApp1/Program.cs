6528cf30ea0939ba2b92617ac38531adceac610a
qq491059861@163.com <qq491059861@163.com>
2020-03-13 16:14:52 +0800

    第四次作业

---
+++ homework4/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApplication
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> node = Head; node != null; node = node.Next)
+            {
+                action(node.Data);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            int sum = 0, max = intlist.Head.Data, min = intlist.Head.Data;
+            intlist.ForEach(x => Console.WriteLine(x));
+            intlist.ForEach(x => sum += x);
+            intlist.ForEach(x => max = x > max ? x : max);
+            intlist.ForEach(x => min = x < min ? x : min);
+            Console.WriteLine("sum=" + sum);
+            Console.WriteLine("max=" + max);
+            Console.WriteLine("min=" + min);
+        }
+
+    }
+}
---
+++ homework4/ConsoleApp2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public delegate void TickHandler(object sender);
+    public delegate void AlarmHandler(object sender);
+    public class Clock
+    {
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+        public int Second { get; set; }
+        public void Start()
+        {
+            while (true)
+            {
+                OnTick(this);
+                if (Second % 60 == 0)
+                    OnAlarm(this);
+            }
+        }
+
+    }
+    public class Form
+    {
+        public Clock clock = new Clock();
+        public Form()
+        {
+            clock.OnTick += new TickHandler(Tick);
+            clock.OnAlarm += new AlarmHandler(Alarm);
+        }
+        void Tick(object sender)
+        {
+            System.Threading.Thread.Sleep(1000);
+            clock.Second += 1;
+            Console.WriteLine("过了" + clock.Second + "秒");
+        }
+        void Alarm(object sender)
+        {
+            Console.WriteLine("响铃！");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form form = new Form();
+            form.clock.Start();
+        }
+    }
+}
