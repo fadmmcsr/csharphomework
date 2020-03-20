976568fe8c82066ad7df52daf4e5462b1e5e411e
qq491059861@163.com <qq491059861@163.com>
2020-03-20 21:58:34 +0800

    第五次作业（未完成）

---
+++ homework5/ConsoleApp1/OrderService.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class OrderService
+    {
+        public List<Order> orders = new List<Order>();
+        public bool DeleteOrder(Order order)
+        {
+            if (!orders.Contains(order))
+                return false;
+            orders.Remove(order);
+            return true;
+        }
+        public bool AddOrder(Order order)
+        {
+            if (orders.Contains(order))
+                return false;
+            orders.Add(order);
+            return true;
+        }
+        public bool UpdateOrder(Order order)
+        {
+            foreach (Order o in orders)
+            {
+                if (o.ID == order.ID)
+                {
+                    int index = orders.IndexOf(o);
+                    orders[index] = order;
+                    return true;
+                }
+            }
+            return false;
+        }
+        public List<Order> QueryByID(int ID)
+        {
+            var query = orders.Where(order => order.ID == ID);
+            return query.ToList();
+        }
+        public List<Order> QueryByCustomerID(int customerID)
+        {
+            var query = orders.Where(order => order.CustomerID == customerID);
+            return query.ToList();
+        }
+        public List<Order> QueryByCustomerName(string customerName)
+        {
+            var query = orders.Where(order => order.CustomerName == customerName);
+            return query.ToList();
+        }
+        public List<Order> QueryByItemName(string itemName)
+        {
+            var query = orders.Where(order =>
+            {
+                foreach (Order o in orders)
+                {
+                    foreach (OrderItem i in o.items)
+                    {
+                        if (i.Name == itemName)
+                            return true;
+                    }
+                }
+                return false;
+            });
+            return query.ToList();
+        }
+        public List<Order> QueryBy(int method,string query)
+        {
+            if (method > 4 || method < 1)
+                return null;
+            switch (method)
+            {
+                case 1:
+                    int ID = Convert.ToInt32(query);
+                    return QueryByCustomerID(ID);
+                case 2:
+                    int customerID = Convert.ToInt32(query);
+                    return QueryByCustomerID(customerID);
+                case 3:
+                    return QueryByCustomerName(query);
+                default:
+                    return QueryByItemName(query);
+            }
+        }
+        public List<Order> SortBy(int method)
+        {
+            if (method > 4 || method < 1)
+                return orders;
+            List<Order> list = new List<Order>();
+            orders.ForEach(order => list.Add(order));
+            switch (method)
+            {
+                case 1:
+                    list.Sort((o1, o2) => string.Compare(o2.CustomerName, o1.CustomerName));
+                    return list;
+                case 2:
+                    list.Sort((o1, o2) => o2.CustomerID - o1.CustomerID);
+                    return list;
+                case 3:
+                    list.Sort((o1, o2) => DateTime.Compare(o2.DateTime, o1.DateTime));
+                    return list;
+                default:
+                    list.Sort((o1, o2) => o2.ID - o1.ID);
+                    return list;
+            }
+        }
+    }
+}
---
+++ homework5/ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static OrderService orderService = new OrderService();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("欢迎使用订单管理系统！请选择服务：\n1.添加订单\t2.更改订单\t3.删除订单\t4.查询订单\t\t0.退出");
+            bool quit = false;
+            bool success;
+            int id = 1;
+            int customerID = 1;
+            while (!quit)
+            {
+                int service = Convert.ToInt32(Console.ReadLine());
+                switch (service)
+                {
+                    case 1:
+                        Console.WriteLine("请输入订单内容：");
+                        string address = Console.ReadLine();
+                        string customerName = Console.ReadLine();
+                        Order order = new Order(id, address, customerName, customerID);
+                        foreach (Order o in orderService.orders)
+                        {
+                            if (o.CustomerName != customerName)
+                                continue;
+                            else
+                                customerID--;
+                                break;
+                        }
+                        id++;
+                        customerID++;
+                        success = orderService.AddOrder(order);
+                        if (success)
+                        {
+                            Console.WriteLine("添加成功！");
+                            break;
+                        }
+                        Console.WriteLine("添加失败！");
+                        break;
+                    case 2:
+                        Console.WriteLine("请输入订单编号：");
+                        int currentID = Convert.ToInt32(Console.ReadLine());
+                        string newAddress = Console.ReadLine();
+                        success = false;
+                        foreach (Order o in orderService.orders)
+                        {
+                            if (o.ID == currentID)
+                            {
+                                order = new Order(currentID, newAddress, o.CustomerName, o.CustomerID);
+                                success = orderService.UpdateOrder(order);
+                                break;
+                            }
+                        }
+                        if (success)
+                        {
+                            Console.WriteLine("修改成功！");
+                            break;
+                        }
+                        Console.WriteLine("修改失败！");
+                        break;
+                    case 3:
+                        Console.WriteLine("请输入订单编号：");
+                        currentID = Convert.ToInt32(Console.ReadLine());
+                        foreach (Order o in orderService.orders)
+                        {
+                            if (o.ID == currentID)
+                            {
+                                order = new Order(currentID, o.Address, o.CustomerName, o.CustomerID);
+                                success = orderService.DeleteOrder(order);
+                            }
+                        }
+                        break;
+                }
+                Console.WriteLine();
+                Console.ReadLine();
+            }
+        }
+    }
+}
